4453501b0487446544f75422be962679e3e3d613
AdisGelici <adisgelici@gmail.com>
2026-05-26 03:35:26 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
-
-  res.setHeader("Access-Control-Allow-Origin", "https://addico.nl");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
